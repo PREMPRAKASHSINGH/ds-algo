3d255050bb67cfeeca50c215fe48f0701273e094
premprakashsingh <premsingh.singh386@gmail.com>
2017-09-19 01:12:34 +0530

    fomatted code for binary search using recursion

--- binary_search.cpp
+++ binary_search.cpp
@@ -21,13 +21,13 @@ int main() {
 
   int list[] = {0,1,2,3,4,5,6,7,8,9};
   int key, index;
-	cout<<"enter key to search in the list ";
-	cin>>key;
-	index = binarySearch(list, 0, 9, key);
-	if(key == -1){
+  cout<<"enter key to search in the list ";
+  cin>>key;
+  index = binarySearch(list, 0, 9, key);
+  if(key == -1){
     cout<<key<<" not found in the list.";
-	}else{
+  }else{
     cout<<key<<" found at index "<<index;
-	}
-	return 0;
+  }
+  return 0;
 }
